c47d0472ab9210a246abd94b0361d9b5b53659e3
Wei4 Wang <wei4.wang@intel.com>
2023-05-19 13:50:27 +0800

    [OpenScreen] Make ServicePublisher::Observer maintained only by ServicePublisherImpl
    Currently, ServicePublisher::Observer is maintained by both
    ServicePublisherImpl and DnsSdPublisherClient, which is confusing.
    This CL makes ServicePublisher::Observer maintained only by
    ServicePublisherImpl.
    
    Bug:none
    Change-Id: Id02df9053b045d77448c528e6e01ff47b8c39a45
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/4548267
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>

--- osp/impl/dns_sd_service_publisher_factory.cc
+++ osp/impl/dns_sd_service_publisher_factory.cc
@@ -22,8 +22,7 @@ std::unique_ptr<ServicePublisher> ServicePublisherFactory::Create(
     const ServicePublisher::Config& config,
     ServicePublisher::Observer* observer,
     TaskRunner* task_runner) {
-  auto dns_sd_client =
-      std::make_unique<DnsSdPublisherClient>(observer, task_runner);
+  auto dns_sd_client = std::make_unique<DnsSdPublisherClient>(task_runner);
   auto publisher_impl = std::make_unique<ServicePublisherImpl>(
       observer, std::move(dns_sd_client));
   publisher_impl->SetConfig(config);
