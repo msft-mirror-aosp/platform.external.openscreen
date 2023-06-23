3192a714bfa30cd5b2017c50e5593e2033299134
mark a. foltz <mfoltz@chromium.org>
2023-06-22 17:00:11 -0700

    [Open Screen] Bulk-update license headers.
    The license header presubmit check enforces the new license header
    format which removes the string "All Rights Reserved."  Chromium was
    updated but not our repo, so doing this now so that unrelated changes
    aren't blocked on license headers.
    
    To pass cpplint, this also fixes a number of IWYU issues and also
    adds `noexcept` to move operators.
    
    Bug: b/288403513
    Change-Id: I90b2e58dcc2bdd1762129c7b5894e3a2730da51b
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/4638621
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>

--- cast/streaming/compound_rtcp_parser.cc
+++ cast/streaming/compound_rtcp_parser.cc
@@ -1,10 +1,11 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "cast/streaming/compound_rtcp_parser.h"
 
 #include <algorithm>
+#include <utility>
 
 #include "cast/streaming/packet_util.h"
 #include "cast/streaming/rtcp_session.h"
--- cast/streaming/mock_compound_rtcp_parser_client.h
+++ cast/streaming/mock_compound_rtcp_parser_client.h
@@ -1,9 +1,11 @@
-// Copyright 2019 The Chromium Authors. All rights reserved.
+// Copyright 2019 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CAST_STREAMING_MOCK_COMPOUND_RTCP_PARSER_CLIENT_H_
 #define CAST_STREAMING_MOCK_COMPOUND_RTCP_PARSER_CLIENT_H_
+
+#include <vector>
 
 #include "cast/streaming/compound_rtcp_parser.h"
 #include "gmock/gmock.h"
