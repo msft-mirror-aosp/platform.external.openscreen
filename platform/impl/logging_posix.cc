25c249d476ab68bf514fad78b60309d302c6b2c7
Vincent Chiang <vincentchiang@chromium.org>
2024-06-18 17:04:32 +0000

    [Open Screen] Optimize logging calls
    This is a follow-up change to logging changes in the chromium repo.
    IsLoggingOn shouldn't directly make calls to strlen, and should get the
    length of the file in compile time. See bug for more details.
    
    Bug: 331984915
    Change-Id: Ib6c6738e7a88933817e66410b0c180598875039d
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/5548142
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>

--- platform/impl/logging_posix.cc
+++ platform/impl/logging_posix.cc
@@ -85,7 +85,7 @@ LogLevel GetLogLevel() {
   return g_log_level;
 }
 
-bool IsLoggingOn(LogLevel level, const char* file) {
+bool IsLoggingOn(LogLevel level, const std::string_view file) {
   // Possible future enhancement: Use glob patterns passed on the command-line
   // to use a different logging level for certain files, like in Chromium.
   return level >= g_log_level;
