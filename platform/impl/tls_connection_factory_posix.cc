b28cd90a53f5aed850b00ef66cd6b177853e0f5a
saralin <saralin@google.com>
2026-07-22 07:22:35 +0000

    Fix null-pointer dereference in TlsConnectionFactory Posix
    SSL_get_peer_certificate can return nullptr if the opposing client does not
    provide a certificate (which is standard behavior for simple connecting tests).
    When it returned nullptr, it caused a fatal segmentation fault due to a missing
    null check prior to dereferencing the X509 pointer.
    
    This patch adds a basic null check returning Error::Code::kErrCertsMissing.
    
    Bug: None
    Change-Id: Ibfda50c990210f67b4d53fff1c8b6236b3501c37
    Fixes: None
    Reviewed-on: https://chromium-review.googlesource.com/c/openscreen/+/8130184
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>

--- platform/impl/tls_connection_factory_posix.cc
+++ platform/impl/tls_connection_factory_posix.cc
@@ -36,6 +36,9 @@ namespace {
 
 ErrorOr<std::vector<uint8_t>> GetDEREncodedPeerCertificate(const SSL& ssl) {
   X509* const peer_cert = SSL_get_peer_certificate(&ssl);
+  if (!peer_cert) {
+    return Error::Code::kErrCertsMissing;
+  }
   ErrorOr<std::vector<uint8_t>> der_peer_cert =
       ExportX509CertificateToDer(*peer_cert);
   X509_free(peer_cert);
